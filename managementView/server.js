c068155223f96674040fccaf38caf9c5e81988c1
calgara12 <michael.polzer@gmx.net>
2022-01-04 12:43:56 +0100

    Add all api functions for tables & users

--- managementView/server.js
+++ managementView/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-
-const app = express();
-
 const port = process.env.PORT || 3001;
 
+const app = express();
+const tables = require('./routes/tables')
+const users = require('./routes/users')
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
 app.use(express.static(__dirname + '/dist/managementView'));
+//routing
+app.use('/api/tables', tables)
+app.use('/api/users', users)
+
+
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
