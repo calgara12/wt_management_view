931a1955228b43d16cfe90b2ab12840923b6b3d8
Bernhard Suppanz <besuppanz@edu.aau.at>
2022-01-06 18:09:48 +0100

    add db pool

--- managementView/pool.js
+++ managementView/pool.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 
-let cfg = require('./config.json')
+let cfg = require('./config.json');
 
 let pool = new Pool(cfg.database);
+
 pool.connect();
 module.exports = pool;
